b87dd9bee983d3a509ebded69f62907264e82236
[Antonio Pérez Díaz] <[antonioperezdiaz007@gmail.com]>
2026-05-15 17:23:37 +0200

    Configuración del pom y del JPAUtil

---
+++ src/main/java/util/JPAUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("$objectdb/db/red_electrica.odb");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
